baa7130cb3c27b39e283d7b023aae5ca60eb99d4
jasmine_r <joycesdaughter82@gmail.com>
2021-07-22 15:19:36 -0500

    Completeddrawing exercise

---
+++ main.py
@@ -0,0 +1,53 @@
+import turtle as turtle
+# Hexagon
+turtle.pencolor("orange")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("pink")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("green")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("blue")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("yellow")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("purple")
+turtle.forward(100)
+turtle.right(60)
+
+# Square
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+# star
+turtle.forward(110)
+turtle.left(216)
+
+turtle.forward(110)
+turtle.left(216)
+
+turtle.forward(110)
+turtle.left(216)
+
+turtle.forward(110)
+turtle.left(216)
+
+turtle.forward(110)
+turtle.left(216)
